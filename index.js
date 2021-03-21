266b4c6c9e3b537b910798828e970eaab11aa566
Ruslan Bekshenev <rbekshenev23@gmail.com>
2021-03-21 13:14:48 +0500

    добавление навигации и стилей

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
+app.use(express.static('public'))
 
 app.get('/', (req, res, next) => {
   res.render('index')
