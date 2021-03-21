024d6c705683aa395448795a749fc69d66dc68d0
Ruslan Bekshenev <rbekshenev23@gmail.com>
2021-03-21 15:13:49 +0500

    регистрация роутов

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
+const homeRoutes = require('./routes/home')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
 
 const app = express()
 
@@ -16,27 +19,10 @@ app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(express.static('public'))
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
 
-app.get('/', (req, res, next) => {
-  res.render('index', {
-    title: 'Главная страница',
-    isHome: true
-  })
-})
-
-app.get('/add', (req, res) => {
-  res.render('add', {
-    title: 'Добавить курс',
-    isAdd: true
-  })
-})
-
-app.get('/courses', (req, res) => {
-  res.render('courses', {
-    title: 'Курсы',
-    isCourses: true
-  })
-})
 
 const PORT = process.env.PORT || 3000
 
