f930e55bf338b5c2db536f4e2e4136a150194204
Ruslan Bekshenev <rbekshenev23@gmail.com>
2021-03-21 05:01:33 +0500

    Подключение handlebars

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const path = require('path')
+const exphbs = require('express-handlebars')
+
 const app = express()
 
+const hbs = exphbs.create({
+  // шаблон по умолчанию
+  defaultLayout: 'main',
+  // расширение
+  extname: 'hbs',
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+
 app.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+  res.render('index')
 })
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+  
 })
 
 const PORT = process.env.PORT || 3000
