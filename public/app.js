c50f971d30cf6ea29b3f98326387e918a314040b
Ruslan Bekshenev <rbekshenev23@gmail.com>
2021-03-21 23:52:35 +0500

    динамическое изменение корзины

--- public/app.js
+++ public/app.js
@@ -1,9 +1,16 @@
-document.querySelectorAll('.price').forEach(node => {
-  node.textContent = new Intl.NumberFormat('ru-RU', {
+
+const toCurrency = price => {
+  return new Intl.NumberFormat('ru-RU', {
     currency: 'rub',
     style: 'currency'
-  }).format(node.textContent)
+  }).format(price)
+}
+
+document.querySelectorAll('.price').forEach(node => {
+  console.log(toCurrency(+node.textContent))
+  node.textContent = toCurrency(node.textContent)
 })
+
 
 const $card = document.querySelector('#card')
 
@@ -17,7 +24,25 @@ if ($card) {
         method: 'delete',
       }).then(res => res.json())
         .then(card => {
-          console.log(card)
+          if (card.courses.length) {
+            const html = card.courses.map(c => {
+              return `
+              <tr>
+                <td>${c.title}</td>
+                <td>${c.count}</td>
+                <td>
+                  <button class="btn btn-small js-remove" data-id="${c.id}">
+                    Удалить
+                  </button>
+                </td>
+              </tr>
+              `
+            }).join('')
+            $card.querySelector('tbody').innerHTML = html
+            $card.querySelector('.price').textContent = toCurrency(card.price)
+          } else {
+            $card.innerHTML = '<p>Корзина пуста</p>'
+          }
         })
     }
   })
